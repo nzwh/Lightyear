39b697d65df56cf9eeab04572e32ebd4e18bf762
nzwh <ivanisaiahserato@gmail.com>
2022-04-08 17:04:58 +0800

    New files: ping.ts, help.ts, invite.ts
    Created empty commands as basis for loading future commands.

---
+++ commands/core/help.ts
@@ -0,0 +1,14 @@
+   
+    import { Message } from 'discord.js';
+    import SuperClient from '../../extensions/super_client';
+
+    export default {
+        run: async (client : SuperClient, message: Message, args: any[]) => {
+
+        },
+
+        help: {
+            name: "help",
+            alias: ['h']
+        }
+    }
