fedee47942679a6bd6fe52823b0da8864427a8f0
nzwh <ivanisaiahserato@gmail.com>
2022-04-08 16:34:27 +0800

    Add command listening
    Now listens to messages based on a prefix, then loads from the collection.

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import { Collection } from 'discord.js';
+import { Client, Intents, Collection } from 'discord.js';
 import super_client from './extensions/super_client';
+const { prefix } = require('./extensions/preferences.json');
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -25,5 +26,21 @@ client.once('ready', () => {
     if (handler.default) handler = handler.default;
     handler(client);
 })
+
+
+client.on('messageCreate', async (message) => {
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.substring(prefix.length).split(" ");
+    let cmd = client.commands.get(args[0]) || client.commands.get(client.aliases.get(args[0]));
+    if(cmd.default) {
+        cmd = cmd.default;
+        cmd.run(client, message, args.slice(1));
+    }
+    
+}) 
 
 client.login(process.env.TOKEN); 
