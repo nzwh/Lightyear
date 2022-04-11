14fa49b0e70e99d962866e5864378439fc4c85e9
nzwh <ivanisaiahserato@gmail.com>
2022-04-11 23:55:58 +0800

    New: help.ts
    Added a dynamic help command that parses metadata from other commands.

--- commands/core/help.ts
+++ commands/core/help.ts
@@ -1,10 +1,30 @@
    
-    import { Message } from 'discord.js';
+    import Discord, { Message } from 'discord.js';
     import SuperClient from '../../extensions/super_client';
+    const { prefix } = require('../../extensions/preferences.json');
 
     export default {
         run: async (client : SuperClient, message: Message, args: any[]) => {
 
+            const catalog = new Discord.MessageEmbed()
+                .setAuthor({ name: "Lightyear Commands" })
+                .setDescription(`The prefix for the bot is \`${prefix}\`. Disclaimer: Most of the commands are experimental. Type \`${prefix}help [command]\` for more information.`)
+                .setColor(message.guild!.me!.displayHexColor)
+
+                .setFooter({ text: `Requested by ${message.author.username}`, iconURL: message.author.avatarURL()!.toString() })
+                .setThumbnail(message.guild!.iconURL()!.toString())
+                .setTimestamp()
+                
+            for (let command of client.commands) {
+
+                let command_listing = "";
+                command_listing += `\`${prefix + command[1].default.name}\` | \`${(command[1].default.alias).toString().replace(/,/g, '`, `')}\` \n`;
+                command_listing += `\`⚡\` ${command[1].default.usage} \n\n`
+
+                catalog.addField(`\u200b`, command_listing, true);
+            }
+            
+            message.channel.send({ embeds: [catalog] });
         },
 
         name: 'help',
