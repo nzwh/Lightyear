56158d4ccc8b2719cd31529921674902671ff873
nzwh <ivanisaiahserato@gmail.com>
2022-04-12 22:36:54 +0800

    Fix: help.ts
    Now returns the help of a certain command when given as an argument.

--- commands/core/help.ts
+++ commands/core/help.ts
@@ -5,26 +5,39 @@
 
     export default {
         run: async (client : SuperClient, message: Message, args: any[]) => {
-
-            const catalog = new Discord.MessageEmbed()
-                .setAuthor({ name: "Lightyear Commands" })
-                .setDescription(`The prefix for the bot is \`${prefix}\`. Disclaimer: Most of the commands are experimental. Type \`${prefix}help [command]\` for more information.`)
-                .setColor(message.guild!.me!.displayHexColor)
-
-                .setFooter({ text: `Requested by ${message.author.username}`, iconURL: message.author.avatarURL()!.toString() })
-                .setThumbnail(message.guild!.iconURL()!.toString())
-                .setTimestamp()
-                
-            for (let command of client.commands) {
-
-                let command_listing = "";
-                command_listing += `\`${prefix + command[1].default.name}\` | \`${(command[1].default.alias).toString().replace(/,/g, '`, `')}\` \n`;
-                command_listing += `\`⚡\` ${command[1].default.usage} \n\n`
-
-                catalog.addField(`\u200b`, command_listing, true);
-            }
             
-            message.channel.send({ embeds: [catalog] });
+                if (client.commands.get(args[0]) === undefined) {
+
+                    const catalog = new Discord.MessageEmbed()
+                        .setAuthor({ name: "Lightyear Commands" })
+                        .setDescription(`The prefix for the bot is \`${prefix}\`. Disclaimer: Most of the commands are experimental. Type \`${prefix}help [command]\` for more information.`)
+                        .setColor(message.guild!.me!.displayHexColor)
+
+                        .setFooter({ text: `Requested by ${message.author.username}`, iconURL: message.author.avatarURL()!.toString() })
+                        .setThumbnail(message.guild!.iconURL()!.toString())
+                        .setTimestamp()
+                        
+                    for (let command of client.commands) {
+                        let command_listing = "";
+                        command_listing += `\`${prefix + command[1].default.name}\` | \`${(command[1].default.alias).toString().replace(/,/g, '`, `')}\` \n`;
+                        command_listing += `\`⚡\` ${command[1].default.usage} \n\n`
+
+                        catalog.addField(`\u200b`, command_listing, true);
+                    }
+                    
+                    message.reply({ embeds: [catalog], allowedMentions: { repliedUser: false } });
+                } else {
+
+                    const command = client.commands.get(args[0]);
+                    const command_info = new Discord.MessageEmbed()
+                        .setTitle(prefix + command.default.name)
+                        .setDescription(`> ${command.default.usage} \n> \`⚡\` Aliases: \`${(command.default.alias).toString().replace(/,/g, '`, `')}\``)
+                        .setColor(message.guild!.me!.displayHexColor)
+
+                        .setFooter({ text: `Requested by ${message.author.username}` })
+                        .setTimestamp()
+                    message.reply({ embeds: [command_info], allowedMentions: { repliedUser: false } });
+                }
         },
 
         name: 'help',
